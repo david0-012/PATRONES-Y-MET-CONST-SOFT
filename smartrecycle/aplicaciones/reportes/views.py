7876bac080133d028745b316f3a6146c7dc2c09f
AlanSantiagoVasquezAriza <alanvasquez.sa@academia.umb.edu.co>
2024-04-28 22:41:45 -0500

    Formulario reporte
    Formulario reporte basico

--- smartrecycle/aplicaciones/reportes/views.py
+++ smartrecycle/aplicaciones/reportes/views.py
@@ -1,6 +1,24 @@
 from django.http import HttpResponse
 from django.template import loader
+from .forms import ReporteForm
+from django.shortcuts import render
 
-def reporteForm(request):
-  template = loader.get_template('formulario.html')
-  return HttpResponse(template.render())
+def reporte(request):
+  data = {
+    'form': ReporteForm() # Instancia del formulario ReporteForm
+  }
+
+  if request.method == 'POST':
+
+    formulario = ReporteForm(data = request.POST)
+
+    if formulario.is_valid():
+
+      formulario.save()
+      data['mensaje'] = "Reporte enviado con exito"
+
+    else:
+      data['form'] = formulario
+      data['mensaje'] = "Error al enviar reporte"
+
+  return render(request, 'reporte.html', data) # Renderiza el template reporte.html con el formulario ReporteForm como parametro
